7afeb8dda131d97666e14a84cd5a7306c3bda004
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-23 22:32:15 +0600

    Food Shop - React Router project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer />
