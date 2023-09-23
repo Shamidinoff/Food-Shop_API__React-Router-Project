a7f13572c860596d523f75bc797bd836705b468e
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-23 21:59:08 +0600

    Food Shop - React Router project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+
+        <footer className="page-footer blue lighten-2">
+            <div className="footer-copyright">
+                <div className="container">
+                    © {new Date().getFullYear()} Copyright Text
+                    <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                </div>
+            </div>
+        </footer>
+
+    )
+}
+
+export { Footer }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Hello, from Home page</h1>
+}
+
+export { Home }
