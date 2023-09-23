a2e7ab51f93d1c75035eb8ceddedc6f90a3bfc35
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-23 22:27:46 +0600

    Food Shop - React Router project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <Header />
-      <main className="container content"></main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className="container content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 }
