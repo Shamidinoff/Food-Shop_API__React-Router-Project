a5ce234090ffac874cd5639df9a775527733e8c7
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-24 12:37:02 +0600

    Food Shop - React Router project

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,11 +1,16 @@
+
 import { CategoryItem } from "./CategoryItem";
 
 function CategoryList({ catalog = [] }) {
-    return <div className="list">
-        {catalog.map(el => (
-            <CategoryItem key={el.idCategory} {...el} />
-        ))}
-    </div>
+
+
+    return (
+        <div className="list">
+            {catalog.map(el => (
+                <CategoryItem key={el.idCategory} {...el} />
+            ))}
+        </div>
+    )
 }
 
 export { CategoryList }
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { getFilteredCategory } from "../api";
 import { Preloader } from "../components/Preloader";
 import { MealList } from "../components/MealList";
@@ -7,6 +7,7 @@ import { MealList } from "../components/MealList";
 function Category() {
     const { name } = useParams();
     const [meals, setMeals] = useState([]);
+    const goBack = useNavigate();
 
     useEffect(() => {
         getFilteredCategory(name).then(data => setMeals(data.meals));
@@ -14,6 +15,7 @@ function Category() {
 
 
     return <>
+        <button className="btn" onClick={() => goBack(-1)}>Go Back</button>
         {!meals.length ? <Preloader /> : <MealList meals={meals} />}
     </>
 }
